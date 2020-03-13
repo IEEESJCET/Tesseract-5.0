5f36ccec7e7f1b8502051eb3c9caa5bd9d8cdaa8
unknown <rithinja@gmail.com>
2020-03-13 16:12:44 +0530

    dates removed

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -225,7 +225,7 @@
     // :: 13.0 Countdown Active Code
     // ****************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2020/03/20 09:00:00", function (event) {
+        $("#clock").countdown("", function (event) {
             $(this).html(event.strftime("<div>%D <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
         });
     }
